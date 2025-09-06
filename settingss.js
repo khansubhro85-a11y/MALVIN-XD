97e7a78c3a52ff5c80161aa3194b6c9472da30bd
khansubhro85-a11y <khansubhro85@gmail.com>
2025-09-06 13:18:04 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~V3cDnABB#d0OSiP1UkDtvwoHWans8JWMZFF5das8aHZOT3oNfWP0"
+  SESSION_ID:malvin~ZuMXgQja#FD3nrqGeM2GUaQlDNyyk099erpneVYU_n1Qn2sFnxrs "malvin~V3cDnABB#d0OSiP1UkDtvwoHWans8JWMZFF5das8aHZOT3oNfWP0"
   OWNER_NUMBER: "263714757857", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
